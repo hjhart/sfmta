c3a0d8bd9a8257567df04327ccf87ee33e715a4f
James Hart <hjhart@gmail.com>
2013-07-16 09:37:07 -0700

    multiplate search now in effect

--- checker.rb
+++ checker.rb
@@ -2,41 +2,50 @@ class Citation
   attr_accessor :id, :issue_date, :violation_code, :violation_name, :amount, :plate_number
 end
 
-
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get('https://prodpci.etimspayments.com/pbw/include/sanfrancisco/input.jsp')
-plate_number = '6WAK359'
+plate_numbers = %w{JEDI001}
+plate_numbers.each do |plate_number|
+  puts "Checking #{plate_number}"
+  page = agent.get('https://prodpci.etimspayments.com/pbw/include/sanfrancisco/input.jsp')
 
-plate_form = page.form('inputForm')
-plate_form.plateNumber = plate_number
+  plate_form = page.form('inputForm')
+  plate_form.plateNumber = plate_number
 
-# TODO: State plates other than california
-# google_form.statePlate = 'California'
+  # TODO: State plates other than california
+  # google_form.statePlate = 'California'
 
-page = agent.submit(plate_form, plate_form.buttons.first)
+  page = agent.submit(plate_form, plate_form.buttons.first)
 
-if page.search('form[name=selectForm]').size > 0
-  puts "Uh oh, you have a citation!"
-  citationsTable = page.search('form[name=selectForm] table')[4]
-  citationsTable.search('tr').each_with_index do |tableRow, index|
-    columns = tableRow.search('td')
-    if index == 0
-      puts "The column text says #{columns[1].text}"
-      puts "Skipping the first index!"
-      next
+  if page.search('form[name=selectForm]').size > 0
+    puts "Uh oh, you have a citation!"
+    citationsTable = page.search('form[name=selectForm] table')[4]
+    citationsTable.search('tr').each_with_index do |tableRow, index|
+      columns = tableRow.search('td')
+
+      if index == 0
+        puts "The column text says #{columns[1].text}"
+        puts "Skipping the first index!"
+        next
+      end
+
+      if tableRow.text[/Total Payment/i]
+        puts "Skipping the total row"
+        next
+      end
+
+      citation = Citation.new()
+      citation.id = columns[1].text
+      citation.issue_date = columns[2].text
+      citation.violation_code = columns[3].text
+      citation.violation_name = columns[4].text
+      citation.amount = columns[5].text
+      citation.plate_number = plate_number
+      citation.plate_state = "CA"
+      pp citation
     end
-    citation = Citation.new()
-    citation.id = columns[1].text
-    citation.issue_date = columns[2].text
-    citation.violation_code = columns[3].text
-    citation.violation_name = columns[4].text
-    citation.amount = columns[5].text
-    citation.plate_number =
-    pp citation
+  else
+    puts "Congratulations! You don't have any citations!"
   end
-else
-  puts "Congratulations! You don't have any citations!"
 end
-
